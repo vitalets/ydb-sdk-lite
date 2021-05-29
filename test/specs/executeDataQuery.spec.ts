bb30bc492ad0f2824916f39a77c3eca6d723de2b
Vitaliy Potapov <noginsk@rambler.ru>
2021-05-29 18:58:22 +0300

    test cache query on server

--- test/specs/executeDataQuery.spec.ts
+++ test/specs/executeDataQuery.spec.ts
@@ -8,8 +8,11 @@ describe('executeDataQuery', () => {
       VALUES ("${userId}", "Alice", true, Datetime("2021-04-17T09:48:19Z"))
     `);
 
-    const query = `SELECT id, name, isAdmin, createdAt FROM users WHERE id = "${userId}"`;
-    const [ rows ] = await ydb.executeDataQuery(query, {}, Ydb.AUTO_TX_RO);
+    const query = `
+      DECLARE $userId AS String;
+      SELECT id, name, isAdmin, createdAt FROM users WHERE id = $userId;
+    `;
+    const [ rows ] = await ydb.executeDataQuery(query, { $userId: userId }, Ydb.AUTO_TX_RO);
 
     assert.deepEqual(rows, [{
       id: userId,
@@ -30,5 +33,13 @@ describe('executeDataQuery', () => {
     await assert.rejects(promise, /Operation 'Upsert' can't be performed in read only transaction/);
   });
 
+  it('cache query on server', async () => {
+    const query = `
+      DECLARE $userId AS String;
+      SELECT id, name, isAdmin, createdAt FROM users WHERE id = $userId;
+    `;
+    const [ rows ] = await ydb.executeDataQuery(query, { $userId: 'foo' }, Ydb.AUTO_TX_RO, {}, { keepInCache: true });
+    assert.deepEqual(rows, []);
+  });
 });
 
