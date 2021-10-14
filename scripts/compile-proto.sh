b943b53c0172ead230c10cc7ea4db32d0946ca4a
Vitaliy Potapov <noginsk@rambler.ru>
2021-10-14 12:10:17 +0300

    proto: build only runtime services

--- scripts/compile-proto.sh
+++ scripts/compile-proto.sh
@@ -5,16 +5,23 @@
 OUT_DIR=./proto
 
 mkdir -p $OUT_DIR
-# rm -rf $OUT_DIR/**
 
-# js
-pbjs \
-  -t static-module \
-  -r ydb-sdk \
-  -w commonjs \
-  -p . \
-  `ls ydb-api-protos-master/*.proto` \
+# include only protos for runtime to get smaller size
+outfile=bundle-runtime
+pbjs -t static-module -r ydb-sdk -w commonjs -p . \
+  ydb-api-protos-master/ydb_table_v1.proto \
+  ydb-api-protos-master/ydb_scripting_v1.proto \
   > proto/bundle.js
 
-# ts
 pbts -o $OUT_DIR/bundle.d.ts $OUT_DIR/bundle.js
+
+# full build (tbd)
+# outfile=bundle-full
+# pbjs -t static-module -r ydb-sdk -w commonjs -p . \
+#   `ls ydb-api-protos-master/*.proto` \
+#   > proto/$outfile.js
+
+# pbts -o $OUT_DIR/$outfile.d.ts $OUT_DIR/$outfile.js
+
+
+
