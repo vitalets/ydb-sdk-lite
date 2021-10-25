df8e8396eb286cb56b7c534ec0d6852a17f563d2
Vitaliy Potapov <noginsk@rambler.ru>
2021-10-25 18:02:22 +0300

    fix duplication error messages

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,10 @@
 export type DropFirst<T extends unknown[]> = T extends [any, ...infer U] ? U : never;
 
 export function appendMessageToError(e: Error, message: string) {
-  e.message = [ e.message, message ].join(' ');
-  e.stack = `${e.name}: ${e.message}\n${(e.stack || '').split('\n').slice(1).join('\n')}`;
+  if (!/^\s/.test(message)) message = ` ${message}`;
+  const newMessage = `${e.message}${message}`;
+  e.stack = `${e.name}: ${newMessage}\n${(e.stack || '').split('\n').slice(1).join('\n')}`;
+  // important to assign e.message AFTER e.stack (to avoid duplication in stack)
+  e.message = newMessage;
   return e;
 }
